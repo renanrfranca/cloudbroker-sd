cb06b9a39972bb2775bac9944602ab60623da85f
João Vitor de Sá <jvsms13@gmail.com>
2019-01-31 14:08:13 -0200

    iniciando a cagada

---
+++ cloudbroker/recebe.php
@@ -0,0 +1,24 @@
+<?php 
+/*
+    JSON RECEBIDO DO POST VEM COM TODOS RECURSOS. Modelo:
+
+    { provedor: xxx
+        {
+            recurso 1 lalala
+        }, 
+        {
+            recurso 2...
+        }, 
+        {
+            recurso 3...
+        }, 
+        
+    
+    }
+*/
+    if(!isset($_POST)){
+        die("algo deu errado...");
+    }
+
+    $acutjason=json_decode($_POST[0]);
+?>
