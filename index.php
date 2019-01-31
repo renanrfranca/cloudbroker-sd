593fc214f5198c7ea5d76ae004048c7027b5bb77
Renan Rossignatti de França <renanrfranca@gmail.com>
2019-01-30 23:25:40 -0200

    Using relative paths

--- index.php
+++ index.php
@@ -1,1 +1,3 @@
 <p1>Cloud Broker</p1>
+
+<a href="/provedores/divulgar.php?id=1">Provedor 1</a>
--- provedores/index.php
+++ provedores/index.php
@@ -1,6 +1,6 @@
     <h3>Divulgar</h3>
 
-    <a href="http://localhost:8000/provedores/divulgar.php?id=1">Provedor 1</a>
-    <a href="http://localhost:8000/provedores/divulgar.php?id=2">Provedor 2</a>
-    <a href="http://localhost:8000/provedores/divulgar.php?id=3">Provedor 3</a>
-    <a href="http://localhost:8000/provedores/divulgar.php?id=4">Provedor 4</a>
+    <a href="/provedores/divulgar.php?id=1">Provedor 1</a>
+    <a href="/provedores/divulgar.php?id=2">Provedor 2</a>
+    <a href="/provedores/divulgar.php?id=3">Provedor 3</a>
+    <a href="/provedores/divulgar.php?id=4">Provedor 4</a>
